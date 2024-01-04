1ae8003d0fdeea0d51d8421744ee34fbe1e9fb36
TheQueen125 <155658559+TheQueen125@users.noreply.github.com>
2024-01-04 23:04:10 +0530

    Add files via upload

---
+++ switch1.java
@@ -0,0 +1,28 @@
+class switch1
+{
+ public static void main(String[]args)
+  {
+   int a=5;
+   switch(a)
+    {
+	 case 1:System.out.println("one");
+	         break;
+	 case 2:System.out.println("two");
+            break;
+     case 3: System.out.println("Three");
+             break;
+     case 4:System.out.println("four");
+             break;
+     case 5:System.out.println("five");
+             break;
+      case 6: System.out.println("Six");
+              break;
+      case 7:System.out.println("seven");
+             break;
+      case 8: System.out.println("eight");
+              break;
+      default:System.out.println("wrong choice");
+    }
+   }
+}   
+	 
